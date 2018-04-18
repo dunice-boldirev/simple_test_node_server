9b4bef82c0f3067b7fa611339bad27c008340ea4
Max Boldirev <max.boldirev@dunice.ru>
2018-04-18 16:54:14 +0300

    my commit

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ var express = require("express"),
     errorHandler = require('errorhandler'),
     methodOverride = require('method-override'),
     hostname = process.env.HOSTNAME || 'localhost',
-    port = parseInt(process.env.PORT, 10) || 4567,
+    port = parseInt(process.env.PORT, 10) || 8080,
     publicDir = process.argv[2] || __dirname + '/public',
     path = require('path');
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(publicDir, "/index.html"));
-});
+
 
 app.use(methodOverride());
 app.use(bodyParser.json());
@@ -24,6 +22,10 @@ app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
 }));
+
+app.all("*", function (req, res) {
+  res.sendFile(path.join(publicDir, "/index.html"));
+});
 
 console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
 app.listen(port, hostname);
